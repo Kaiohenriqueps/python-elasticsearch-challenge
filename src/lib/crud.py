93831132a92c93e8f9e9b4b47095fb6a6701bb4b
Kaiohenriqueps <kh.silva@hotmail.com>
2021-12-05 15:33:48 -0300

    crud update

--- src/lib/crud.py
+++ src/lib/crud.py
@@ -5,16 +5,5 @@ def connect_es():
     return Elasticsearch(hosts=["elasticsearch:9200"])
 
 
-def search_doc(es, index, data):
-    return es.search(
-        index=index,
-        body={"query": {"bool": {"must": [{"terms": {"city": data}}]}}},
-    )
-
-
-def query_all(es, index):
-    return es.search(index=index, body={"query": {"match_all": {}}})
-
-
 def search_docs(es, index, query):
     return es.search(index=index, body=query)
